9c36b5b70c6cfe6de857854ac149baf310c2cbfd
martin-git <martin98239823@yahoo.de>
2016-07-18 03:34:44 -0500

    first running json requests with unformated output

--- google_flights.py
+++ google_flights.py
@@ -1,18 +1,40 @@
-import urllib
 import json
-import itertools
 import smtplib
+import requests
 
 # Set some variables
 
-keyfile = 'key1.txt'
+deplist = ["2016-12-18"] # list of departure dates
 
-listofstartdates = ["2016-12-18", "2016-12-19"]
+retlist = ["2017-01-18"] # list of return dates
 
-listofenddates = ["2017-01-18", "2017-01-19"]
+origin = "MEX" # IATA designator of origin airport
 
-minprice = []
-flightsfound = []
+destination = "INN" # IATA designator of destination airport
+
+nAdults = 1 # number of adult passangers
+
+nChildren = 0 # number of child passangers
+
+maxStops = 1 # maximum number of stops
+
+permittedCarrier = [] # Only flights using these carriers will be returned (list of 2-letter IATA airline designators)
+
+prohibitedCarrier = [] # Flights using these carriers will be ignored (list of 2-letter IATA airline designators)
+
+saleCountry = "MX" # IATA country code representing the point of sale
+
+ticketingCountry = "MX" # IATA country code representing the point of ticketing
+
+nSolutions = 50 # maximum number of results (max = 500)
+
+keyfile = 'key1.txt' # file containing api key
+
+# IATA Codes: https://en.wikipedia.org/wiki/International_Air_Transport_Association_code
+
+
+
+
 
 
 def read_key(keyfile):
@@ -20,48 +42,124 @@ def read_key(keyfile):
         apikey = myfile.read().replace('\n', '')
     return apikey
 
+def checkprice(url, saleCountry, ticketingCountry, nSolutions, dep, ret, origin, destination, nAdults, nChildren,
+               maxStops, permittedCarrier, prohibitedCarrier):
+
+    # request = {
+    #     "request": {
+    #         "slice": [
+    #             {
+    #                 "origin": origin,
+    #                 "destination": destination,
+    #                 "date": dep
+    #             },
+    #             {
+    #                 "origin": destination,
+    #                 "destination": origin,
+    #                 "date": ret
+    #             }
+    #         ],
+    #         "passengers": {
+    #             "adultCount": nAdults,
+    #             "infantInLapCount": 0,
+    #             "infantInSeatCount": 0,
+    #             "childCount": 0,
+    #             "seniorCount": 0
+    #         },
+    #         "solutions": 500,
+    #         "maxPrice": "NZD5700",
+    #         "refundable": False
+    #     }
+    # }
+
+    request = {
+        "request": {
+            "passengers": {
+                "kind": "qpxexpress#passengerCounts",
+                "adultCount": nAdults,
+                "childCount": nChildren,
+                "infantInLapCount": 0,
+                "infantInSeatCount": 0,
+                "seniorCount": 0
+            },
+            "slice": [
+                {   # departure flight
+                    "kind": "qpxexpress#sliceInput",
+                    "origin": origin,
+                    "destination": destination,
+                    "date": dep,
+                    "maxStops": maxStops,
+                    "permittedCarrier": permittedCarrier,
+                    "prohibitedCarrier": prohibitedCarrier
+                },
+                {   # return flight
+                    "kind": "qpxexpress#sliceInput",
+                    "origin": destination,
+                    "destination": origin,
+                    "date": ret,
+                    "maxStops": maxStops,
+                    "permittedCarrier": permittedCarrier, # list of 2-letter IATA airline designators
+                    "prohibitedCarrier": prohibitedCarrier # list of 2-letter IATA airline designators
+                }
+            ],
+            "saleCountry": saleCountry,
+            "ticketingCountry": ticketingCountry,
+            "refundable": False,
+            "solutions": nSolutions
+        }
+    }
+
+    jsonreq = json.dumps(request)
+    print(jsonreq)
+
+    try:
+        r = requests.post(url, data=jsonreq, headers={"Content-Type": "application/json"})
+        print(r.json())
+        result = r.json()
+        r.raise_for_status()
+
+    except requests.exceptions.HTTPError as e:
+        print ("Error: " + str(e))
+
+    for trip in result["trips"]["tripOption"]:
+        print(trip["saleTotal"], "(from " + dep + " to " + ret + ")")
+
+        # jede tripOption hat: saleTotal, id,  2 slices (hin und retur flug)!!
+
+
+        # try:
+        # 	req = urllib2.Request(url, jsonreq, {'Content-Type': 'application/json'})
+        # 	flight = urllib2.urlopen(req)
+        # 	response = flight.read()
+        # 	result = json.loads(response)
+        # 	flight.close()
+        #
+        # 	try :
+        # 		for i in result['trips']['tripOption']:
+        # 			#return "Found " + i['saleTotal'] +  "  (from " + startdate + " to " + enddate + ")\n"
+        # 			return (i['saleTotal'], "(from " + startdate + " to " + enddate + ")")
+        # 	except:
+        # 		return ""
+        # 		#return "Nothing found for that price (from " + startdate + " to " + enddate + ")"
+        # except urllib2.HTTPError, err:
+        # 	if err.code == 403:
+        # 		print "API requsts limit exceeded!"
+
+
 def main():
-    apikey=read_key(keyfile)
-    print(apikey)
 
+    url="https://www.googleapis.com/qpxExpress/v1/trips/search?key="+read_key(keyfile)
+    print(url)
 
-
+    for dep, ret in zip(deplist,retlist):
+        print(dep, ret)
+        checkprice(url, saleCountry, ticketingCountry, nSolutions, dep, ret, origin, destination, nAdults, nChildren,
+                   maxStops, permittedCarrier, prohibitedCarrier)
 
 main()
 
 
 # def checkprice(startdate,enddate,minprice):
-#
-# 	url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=%s" % apikey
-#
-# 	request = {
-# 	"request": {
-# 	"slice": [
-# 	{
-# 	"origin": "AKL",
-# 	"destination": "MOW",
-# 	"date": startdate
-# 	},
-# 	{
-# 	"origin": "MOW",
-# 	"destination": "AKL",
-# 	"date": enddate
-# 	}
-# 	],
-# 	"passengers": {
-# 	"adultCount": 2,
-# 	"infantInLapCount": 0,
-# 	"infantInSeatCount": 0,
-# 	"childCount": 0,
-# 	"seniorCount": 0
-# 	},
-# 	"solutions": 500,
-# 	"maxPrice": minprice,
-# 	"refundable": False
-# 	}
-# 	}
-#
-# 	jsonreq = json.dumps(request, encoding = 'utf-8')
 #
 # 	try:
 # 		req = urllib2.Request(url, jsonreq, {'Content-Type': 'application/json'})
